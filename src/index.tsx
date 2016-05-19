113fd82b017e31e35af0ea9fbddfcb19dd9fd360
Nerehta2 <khapaloff@gmail.com>
2016-05-19 13:19:06 +0300

    Create TypeScript+React+Webpack bundle

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReacoDOM from "react-dom";
+
+import { App } from "./components/Hello";
+
+ReacoDOM.render(
+    <App/>,
+    document.getElementById("example")
+);
